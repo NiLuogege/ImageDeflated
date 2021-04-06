75ea9195dba4c87b3e4c565453228692b3ba61c1
luoChen <hank.luo@xianghuanji.com>
2021-04-06 14:27:33 +0800

    创建 plugin 模块，并添加 本地仓库

---
+++ plugin/src/main/groovy/com/niluogege/plugin/ImageDeflatedPlugin.groovy
@@ -0,0 +1,14 @@
+package com.niluogege.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+
+class ImageDeflatedPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        print("flfjlfl;asdfjlfsdfl;sdfsdf")
+    }
+
+}
