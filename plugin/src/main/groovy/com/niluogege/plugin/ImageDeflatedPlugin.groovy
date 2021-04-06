a055092f97a687391b0e2aca7cc6bbc85363895c
luoChen <hank.luo@xianghuanji.com>
2021-04-06 18:06:20 +0800

    ..

--- plugin/src/main/groovy/com/niluogege/plugin/ImageDeflatedPlugin.groovy
+++ plugin/src/main/groovy/com/niluogege/plugin/ImageDeflatedPlugin.groovy
@@ -3,6 +3,7 @@ package com.niluogege.plugin
 import com.niluogege.plugin.extension.ImageDeflatedExtension
 import com.niluogege.plugin.extension.TinyExtension
 import com.niluogege.plugin.extension.WebpExtension
+import com.niluogege.plugin.task.ImageDeflatedTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -19,12 +20,35 @@ class ImageDeflatedPlugin implements Plugin<Project> {
         project.afterEvaluate {
 
             def android = project.extensions.android
-            def imageDeflated = project.extensions.imageDeflated
-            def imageDeflated2 = project.imageDeflated
+            ImageDeflatedExtension imageDeflated = project.extensions.imageDeflated
+            ImageDeflatedExtension imageDeflated2 = project.imageDeflated
 
 
-            print("android=$android imageDeflated=$imageDeflated imageDeflated2=$imageDeflated2")
+            android.applicationVariants.all { variant ->
+                createTask(project, variant)
+            }
+
+            android.buildTypes.all { buildType ->
+                createTask(project, buildType)
+            }
+
+            println("imageDeflated=${imageDeflated.toString()} tiny=${imageDeflated.tiny} open=${imageDeflated.tiny?.open} ")
+            println("imageDeflated2=${imageDeflated2.toString()} tiny=${imageDeflated2.tiny} open=${imageDeflated2.tiny?.open} ")
         }
+
+
     }
 
+
+    private static void createTask(Project project, variant) {
+        def variantName = variant.name.capitalize()
+        def taskName = "imageDeflate$variantName"
+        if (project.tasks.findByName(taskName) == null) {
+            def imageDeflatedTask = project.tasks.create(taskName, ImageDeflatedTask)
+            def mergeResourcesTask = variant.mergeResourcesProvider.get()
+            mergeResourcesTask.dependsOn(imageDeflatedTask)
+
+            print("mergeResourcesTask=${mergeResourcesTask.getName()}")
+        }
+    }
 }
